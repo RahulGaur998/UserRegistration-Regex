d3c0922f7b2102f466891d2c9603fdaaada991e2
RahulGaur998 <gaur.rahul996@gmail.com>
2022-10-02 12:30:35 +0530

    ADD: Added logic for last Name validation and used while loop to take input again if input is not valid

--- src/main/java/com/regex/User.java
+++ src/main/java/com/regex/User.java
@@ -2,6 +2,15 @@ package com.regex;
 
 public class User {
     private String firstName;
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getFirstName() {
         return firstName;
